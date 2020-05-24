0b9fe7247d6616b5a816aaac7f310bdecdee228e
shin usuda <sasasa.24.blue@gmail.com>
2020-05-25 02:11:38 +0900

    Add sign up function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,15 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 import * as express from 'express';
 import {db} from './firestore';
+const cors = require('cors');
 
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
 const app = express();
+app.use(cors());
 
 interface FirestoreUser {
   id: string,
@@ -24,6 +31,29 @@ app.get('/users', async(req, res) => {
   })
 
   res.send(JSON.stringify(users))
+});
+
+app.post('/users/signup', async(req, res) => {
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(req.body.idToken);
+    if (decodedToken.uid) {
+      await db.collection('users').add({
+        name: req.body.name,
+        email: req.body.email,
+        uid: decodedToken.uid,
+      })
+      const data = {
+        status: "success",
+      
+      };
+      res.send(JSON.stringify(data));
+    }
+  } catch {
+    const data = {
+      status: 'fail',
+    };
+    res.send(JSON.stringify(data));
+  }
 });
 
 const usersApi = functions.https.onRequest(app);
